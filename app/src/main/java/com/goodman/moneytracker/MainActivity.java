d2e9ac142da807d967e6556fde007b77f79a443c
Andrey <45331404+vallok@users.noreply.github.com>
2020-05-21 20:56:43 +0300

    Added background, textColor and fondSize for TextView Added logcat for Callback methods

--- app/src/main/java/com/goodman/moneytracker/MainActivity.java
+++ app/src/main/java/com/goodman/moneytracker/MainActivity.java
@@ -1,8 +1,9 @@
 package com.goodman.moneytracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity", "OnStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity", "OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity", "OnDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity", "OnPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity", "OnResume");
     }
 }
